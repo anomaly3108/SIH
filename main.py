65688da4788bf6b49747cd2ee9379af4012dc518
Dushyant <dushyantgoyal28@gmail.com>
2020-02-04 12:38:09 +0000

    output send to webpage !!

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import library
+def bitch(posts):
+    skillist = library.skills()
+    user_skill = posts.split(',')
+    temp,number = library.user(user_skill,skillist)
 
-skillist = library.skills()
-user_skill = library.inp()
-temp,number = library.user(user_skill,skillist)
+    print('based on your skills:')
+    print(' , '.join(user_skill))
 
-print('based on your skills:')
-print(' , '.join(user_skill))
-
-jobs = library.aviral(number)
-
+    jobs = library.aviral(number)
+    return jobs
+'''
 if len(jobs)==0:
     print('no job found')
 elif len(jobs)==1:    
@@ -17,3 +18,4 @@ elif len(jobs)==1:
 else:
     print('The Recommended Job are: ')
     print(' , '.join(jobs))
+'''
