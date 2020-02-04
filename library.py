a55db382d79b48f2d9b91f6bd6dc1e3b19ff73b2
Dushyant <dushyantgoyal28@gmail.com>
2020-02-04 12:37:07 +0000

    changes made

--- library.py
+++ library.py
@@ -1,33 +1,60 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 import csv
+
+
 def aviral(number):
-    jobs=[]
-    with open('jobs.csv', newline='') as File:  
+    jobs = []
+    with open('jobs.csv', newline='') as File:
         reader = csv.reader(File)
-        for i,row in enumerate(reader):
+        for i, row in enumerate(reader):
             if i in number:
                 jobs.append(row[1])
     return jobs
-                    
+
+
 def skills():
-    array=[]
-    with open('jobs.csv', newline='') as File:  
+    array = []
+    with open('jobs.csv', newline='') as File:
         reader = csv.reader(File)
-        for i,row in enumerate(reader):
+        for i, row in enumerate(reader):
             array.append(row[3][1:][:-1].split(","))
     return array
 
 
-def user(inp,skillist):
-    array=[]
-    number=[]
+def user(inp, skillist):
+    array = []
+    number = []
     for x in inp:
-        for i,y in enumerate(skillist):
+        for i, y in enumerate(skillist):
             if x in y:
                 if y not in array:
                     array.append(y)
                     number.append(i)
-    return array,number
+    return array, number
 
-def inp():
-    user_skill = input().split(",")
+
+def inp(posts):
+    user_skill = posts.split(",")
     return user_skill
+
+@app.route('/')
+def student():
+    return render_template('index.html')
+
+
+
+@app.route('/output', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        db_skills = request.form['skillsearch']
+        # db_intrests = request.form['intrest']
+        # db_parents = request.form['parentrecommendation']
+        posts = db_skills
+        inp(posts)
+        return render_template("output.html", post=db_skills)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
