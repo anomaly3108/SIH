6079f980d124966bf88341ab461689bbb9e53282
Dushyant <dushyantgoyal28@gmail.com>
2020-02-05 06:29:40 +0000

    Final Update (1)

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import library
-def bitch(posts):
+def jobs(posts):
     skillist = library.skills()
     user_skill = posts.split(',')
     temp,number = library.user(user_skill,skillist)
-
-    print('based on your skills:')
-    print(' , '.join(user_skill))
-
+    # print('based on your skills:')
+    # print(' , '.join(user_skill))
     jobs = library.aviral(number)
     return jobs
+
+def from_intrests(intrests):
+    skillist = library.skills()
+    user_intrests = intrests.split(',')
+    temp,number = library.user(user_intrests,skillist)
+    jobs_basedon_intrests = library.aviral(number)
+    return jobs_basedon_intrests
+
+def from_parents(parents):
+    skillist = library.skills()
+    user_parents = parents.split(',')
+    temp,number = library.user(user_parents,skillist)
+    jobs_basedon_parents = library.aviral(number)
+    return jobs_basedon_parents
+
 '''
 if len(jobs)==0:
     print('no job found')
